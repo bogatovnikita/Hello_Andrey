1465662754cebb3f0d6e0c6573f3b91d4207542f
bogatovnikita <bogatovnikita@gmail.com>
2021-12-19 09:59:59 +0300

    lesson #1. add two activity

--- app/src/main/java/com/bogatovnikita/helloandrey/MainActivity.kt
+++ app/src/main/java/com/bogatovnikita/helloandrey/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bogatovnikita.helloandrey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sayHello = findViewById<MaterialButton>(R.id.say_hello)
+        sayHello.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/bogatovnikita/helloandrey/SayHelloActivity.kt
@@ -0,0 +1,11 @@
+package com.bogatovnikita.helloandrey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SayHelloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_say_hello)
+    }
+}
